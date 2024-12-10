d8a5aefaa4861b20f64fba1978f4427b16e53a3d
val213 <1042594899@qq.com>
2024-12-10 18:50:56 +0800

    fix：修改合并问题，通过编译并测试达到预期

--- service/libre-user/src/oauth/casdoor.rs
+++ service/libre-user/src/oauth/casdoor.rs
@@ -128,6 +128,7 @@ impl TryFrom<CasdoorUser> for models::NewUser {
         let username = user.name.ok_or(Error::Other("Name is required"))?;
         Ok(
             Self {
+                uid: uuid::Uuid::new_v4(),
                 login: username.clone(),
                 name: user.preferred_username,
                 email: user.email,
@@ -139,23 +140,6 @@ impl TryFrom<CasdoorUser> for models::NewUser {
     }
 }
 
-impl TryFrom<CasdoorUser> for models::NewUser {
-    type Error = crate::oauth::Error;
-
-    fn try_from(user: CasdoorUser) -> Result<Self, Self::Error> {
-        let username = user.name.ok_or(Error::Other("Name is required"))?;
-        Ok(
-            Self {
-                login: username.clone(),
-                name: user.preferred_username,
-                email: user.email,
-                avatar: user.picture,
-                casdoor_id: Some(user.sub),
-                ..Default::default()
-            }
-        )
-    }
-}
 
 pub fn casdoor_config(cfg: &mut web::ServiceConfig) {
     let client_id = env::var("CASDOOR_CLIENT_ID");
@@ -255,8 +239,6 @@ async fn callback(
         Err(models::Error::NotFound) => {
             // Create a new user ==> sign-up
             // models::User::try_from(casdoor_user)?
-                .create(&mut conn)
-                .await?
             models::NewUser::try_from(casdoor_user)?.create(&mut conn).await?
         }
         Err(err) => return Err(err.into()),
