d75c3b73a5da8b08676ef24b05a0b34c27ebcb7c
Samuka007 <samuka007@dragonos.org>
2024-12-10 20:55:11 +0800

    add environment variable to build time

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,19 +1,5 @@
 import type { NextConfig } from 'next'
 
-// const checkEnvironments = () => {
-//   const requiredEnvs = [
-//     "NEXT_PUBLIC_LIBRE_BOOK_BACKEND_URL",
-//     "NEXT_PUBLIC_LIBRE_USER_BACKEND_URL",
-//   ];
-//   requiredEnvs.forEach((env) => {
-//     if (!process.env[env]) {
-//       throw new Error(`Environment variable ${env} is not set`);
-//     }
-//   });
-// };
-
-// checkEnvironments();
-
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   eslint: {
